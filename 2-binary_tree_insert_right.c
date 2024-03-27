7a4e840e7debc067d42b66494b9059f575caa950
Nkiru Arinze <christieswazz@gmail.com>
2024-03-27 14:12:29 -0700

    trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,7 @@
  * Return: a pointer to the created node, or NULL on failure
  *
  * Description: If parent already has a right-child, the new node must take its
- * place, and the old right-child must be set as the right-child of the new node.
+ * place,and the old right-child must be set as the right-child of the new node
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
